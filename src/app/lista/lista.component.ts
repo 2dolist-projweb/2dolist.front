2e9bd959fc065a013f7db92b2d19053408b51f31
Thales Reis <thales.areis@gmail.com>
2019-09-22 23:22:51 -0300

    mudanças na lista

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -1,23 +1,22 @@
-import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
-import { ListaService } from '../services/listas.service';
-import { Lista } from '../models/lista';
-import { Router } from '@angular/router';
+import { Component, EventEmitter, OnInit, Input, Output } from "@angular/core";
+import { ListaService } from "../services/listas.service";
+import { Lista } from "../models/lista";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-lista',
-  templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.css']
+  selector: "app-lista",
+  templateUrl: "./lista.component.html",
+  styleUrls: ["./lista.component.css"]
 })
 export class ListaComponent implements OnInit {
-
   @Input() lista: Lista;
   @Output() deleteLista = new EventEmitter();
-  novoItem: string = '';
-  constructor (private listaService: ListaService, private router: Router) {}
+  novoItem: string = "";
+  constructor(private listaService: ListaService, private router: Router) {}
 
   ngOnInit(): void {
     this.refreshItems();
-  } 
+  }
 
   deleteItem() {
     this.listaService.deleteLista(this.lista.id).subscribe(retorno => {
@@ -33,40 +32,47 @@ export class ListaComponent implements OnInit {
   }
 
   addTarefa() {
-    var task = {
-      descricao: this.novoItem,
-      IdLista: this.lista.id
-    };
+    if (this.novoItem.trim) {
+      var task = {
+        descricao: this.novoItem,
+        IdLista: this.lista.id
+      };
 
-    this.listaService.addItemLista(task).subscribe(retorno => {
-      if (retorno) {
-        this.novoItem = '';
-        this.refreshItems();
-      }
-    })
+      this.listaService.addItemLista(task).subscribe(retorno => {
+        if (retorno) {
+          this.novoItem = "";
+          this.refreshItems();
+        }
+      });
+    }
   }
 
-  bodyClass(): string
-  {
+  bodyClass(): string {
     var cssClass = "";
-    switch(this.lista.prioridade) {
-      case 0: cssClass = "card bg-danger";
-      break;
-      
-      case 1: cssClass = "card bg-warning";
-      break;
+    switch (this.lista.prioridade) {
+      case 0:
+        cssClass = "card bg-danger";
+        break;
 
-      case 2: cssClass = "card bg-info";
-      break;
+      case 1:
+        cssClass = "card bg-warning";
+        break;
 
-      case 3: cssClass = "card bg-info";
-      break;
+      case 2:
+        cssClass = "card bg-info";
+        break;
 
-      case 4: cssClass = "card bg-light";
-      break;
+      case 3:
+        cssClass = "card bg-info";
+        break;
 
-      default: cssClass = "card bg-light";
-      break;
+      case 4:
+        cssClass = "card bg-light";
+        break;
+
+      default:
+        cssClass = "card bg-light";
+        break;
     }
 
     return cssClass;
